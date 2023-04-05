c917dd0bbcb7356a34a1eaf7adc2d0d79a1a605f
password-kim <galaxykim0611@gmail.com>
2023-04-05 17:02:15 +0900

    [Feat] 플레이어 차지 완료 상태 구현.

--- Moonlighter/Assets/Scripts/Player/PlayerStates/AbilityState/PlayerSecondaryActionState/PlayerOnSecondaryActionState.cs
+++ Moonlighter/Assets/Scripts/Player/PlayerStates/AbilityState/PlayerSecondaryActionState/PlayerOnSecondaryActionState.cs
@@ -1,10 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using EnumValue;
 using UnityEngine;
 
 public class PlayerOnSecondaryActionState : PlayerAbilityState
 {
-    
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+
+        player.CurrentState = PlayerStates.OnSecondaryAction;
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (false == inputHandler.SecondaryActionInput)
+        {
+            ChangeState(animator, PlayerStates.OnSecondaryAction, PlayerAnimParams.ONSECONDARYACTION, PlayerAnimParams.IDLE);
+        }
+
+        if (inputHandler.MoveInput != Vector2.zero)
+        {
+            ChangeState(animator, PlayerStates.OnSecondaryAction, PlayerAnimParams.ONSECONDARYACTION, PlayerAnimParams.SECONDARYACTION);
+        }
+    }
 
 
 
