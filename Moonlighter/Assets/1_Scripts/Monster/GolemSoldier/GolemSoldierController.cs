31be778e1edef03236d925b5e2dee92c7ee24e93
password-kim <galaxykim0611@gmail.com>
2023-07-28 15:30:36 +0900

    [Refactor]  DungeonManager 수정

--- Moonlighter/Assets/1_Scripts/Monster/GolemSoldier/GolemSoldierController.cs
+++ Moonlighter/Assets/1_Scripts/Monster/GolemSoldier/GolemSoldierController.cs
@@ -7,25 +7,26 @@ public class GolemSoldierController : MonsterController
 
     protected override bool ShouldBeMoving()
     {
-        float distance = CalculateDistance();
+        float distance = GetDistanceToPlayer();
 
         return CheckRange(_detectRange, distance);
     }
 
     protected override bool ShouldBeAttack()
     {
-        float distance = CalculateDistance();
+        float distance = GetDistanceToPlayer();
 
         return CheckRange(_attackRange, distance);
     }
 
-    private float CalculateDistance()
+    private float GetDistanceToPlayer()
     {
         Vector3 targetPosition = Managers.Dungeon.Player.transform.position;
+
         Vector3 direction = targetPosition - transform.position;
 
         return Vector3.Magnitude(direction);
     }
 
-    private bool CheckRange(float range, float distance) => distance < range ? true : false;
+    private bool CheckRange(float range, float distance) => distance < range;
 }
